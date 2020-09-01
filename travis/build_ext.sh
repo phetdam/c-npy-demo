877f58bd170f6d8559c5bb1124cfae29432f91cc
Derek Huang <djh458@stern.nyu.edu>
2020-09-01 03:06:14 -0400

    update build_ext.sh with extension module build

--- travis/build_ext.sh
+++ travis/build_ext.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 # builds c_numpy_demo.cext, the C extension part of the package, as well as
 # the non-Python extension C code as a shared object.
+# note: DO NOT source this script.
 
 # check if on travis or not
 if ! [ $TRAVIS ]
@@ -8,3 +9,6 @@ then
     echo "WARNING: script intended to be run on Travis CI"
     exit
 fi
+
+# build c_numpy_demo.cext, where package is in directory build
+python3 setup.py build --build-lib build
