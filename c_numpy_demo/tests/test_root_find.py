bf7f88a449ba16a221016dcb5636d028b6871ffc
Derek Huang <djh458@stern.nyu.edu>
2020-09-03 23:39:07 -0400

    succeeded tests of black and bachelier vol on near the money options

--- c_numpy_demo/tests/test_root_find.py
+++ c_numpy_demo/tests/test_root_find.py
@@ -29,8 +29,8 @@ def rf_stop_defaults():
 
 
 @pytest.fixture(scope = "module")
-def options_demo_data():
-    """Creates a small panel of options test data for testing functions with.
+def options_ntm_data():
+    """Creates a small panel of near the money options test data.
     
     Note that the data in each row will be in the parameter order specified by
     :class:`vol_obj_args`. Assumes that there are 365 days in a year. Only uses
@@ -62,17 +62,17 @@ def options_full_data():
 
 
 @pytest.mark.parametrize("method", ["halley", "newton"])
-@pytest.mark.parametrize("guess", [0.5])
+@pytest.mark.parametrize("guess", [0.5, 0.7, 1]) # default guess is 0.5
 @pytest.mark.parametrize("max_pts", [np.inf])
-@pytest.mark.parametrize("debug", [False]) # True to allow C function printf
-def test_c_black_vol(options_demo_data, rf_stop_defaults, method, guess,
-                     max_pts, debug):
-    """Test solving for Black volatility using :func:`options_demo_data` data.
+@pytest.mark.parametrize("py_debug,c_debug", [(False, False)])
+def test_c_ntm_black_vol(options_ntm_data, rf_stop_defaults, method, guess,
+                         max_pts, py_debug, c_debug):
+    """Test solving for Black volatility using :func:`options_ntm_data` data.
     
     Directly calls the ``_black_vol`` function from ``_ivlib.so``.
     
-    :param options_demo_data: :func:`options_demo_data` ``pytest`` fixture.
-    :type options_demo_data: :class:`numpy.ndarray`
+    :param options_ntm_data: :func:`options_ntm_data` ``pytest`` fixture.
+    :type options_ntm_data: :class:`numpy.ndarray`
     :param rf_stop_defaults: :func:``rf_stop_defaults` ``pytest`` fixture.
     :type rf_stop_defaults: tuple
     :param method: Method to use to solve. Only supports ``"halley"`` and
@@ -82,16 +82,21 @@ def test_c_black_vol(options_demo_data, rf_stop_defaults, method, guess,
     :param guess: Starting guess for Black implied volatility.
     :type guess: float
     :param max_pts: Maximum number of data points (rows) from
-        :func:`options_demo_data` to run tests with. Must be positive, and if
-        larger than ``options_demo_data.shape[0]``, will be set to
-        ``options_demo_data.shape[0]`` automatically.
+        :func:`options_ntm_data` to run tests with. Must be positive, and if
+        larger than ``options_ntm_data.shape[0]``, will be set to
+        ``options_ntm_data.shape[0]`` automatically.
     :type max_pts: int
-    :param debug: ``True`` for debugging output (print to ``stdout``) from C
-        function, ``False`` for silence except upon error.
-    :type debug: bool
+    :param py_debug: ``True`` for debugging output (print statements) from
+        Python interpreter, ``False`` for silence. Useful for debugging large
+        chunks of data points at once.
+    :type py_debug: bool
+    :param c_debug: ``True`` for debugging output (print to ``stdout``) from C
+        function, ``False`` for silence except upon error. Useful for debugging
+        single data points.
+    :type c_debug: bool
     """
-    # convert ndarray options_demo_data to tuple of vol_obj_args
-    voas = ndarray2vol_obj_args_tuple(options_demo_data)
+    # convert ndarray options_ntm_data to tuple of vol_obj_args
+    voas = ndarray2vol_obj_args_tuple(options_ntm_data)
     # convert method to int
     method = 0 if "halley" else (1 if "newton" else -1)
     assert method in (0, 1), "method flag must be 0 (halley) or 1 (newton)"
@@ -99,7 +104,7 @@ def test_c_black_vol(options_demo_data, rf_stop_defaults, method, guess,
     # get min of max_pts and length of voas
     n_pts = min(max_pts, len(voas))
     # implied volatility results (struct), values (vols), convergences, flags
-    vol_structs = [None for _ in range(n_pts)]
+    volrs = [None for _ in range(n_pts)]
     vols = np.zeros(n_pts)
     successes = np.zeros(n_pts).astype(bool)
     # can't use numpy.zeros(n_pts).astype(str) since the string gets clipped.
@@ -111,20 +116,96 @@ def test_c_black_vol(options_demo_data, rf_stop_defaults, method, guess,
     for i in range(n_pts):
         vol_args = voas[i]
         # get result struct
-        vol_structs[i] = _ivlib._black_vol(ctypes.byref(vol_args), method,
-                                           guess, *rf_stop_defaults, debug)
+        volrs[i] = _ivlib._black_vol(ctypes.byref(vol_args), method,
+                                     guess, *rf_stop_defaults, c_debug)
         # get actual vol value
-        vols[i] = vol_structs[i].res
+        vols[i] = volrs[i].res
         # get convergence information (True if converged)
-        successes[i] = vol_structs[i].converged
+        successes[i] = volrs[i].converged
         # reason for failure/success (decode bytes object)
-        flags[i] = vol_structs[i].flag.decode()
+        flags[i] = volrs[i].flag.decode()
         # number of iterations required
-        iters[i] = vol_structs[i].iters
-    # print vol values, convergences, number of iterations, and flags
-    print(f"vols:\n{vols}")
-    print(f"successes:\n{successes}")
-    print(f"iters:\n{iters}")
-    print(f"flags:\n{flags}")
-    # test for convergence (might fail sometimes)
+        iters[i] = volrs[i].iters
+    # if py_debug, print vols, convergences, number of iterations, and flags
+    if py_debug == True:
+        print(f"vols:\n{vols}")
+        print(f"successes:\n{successes}")
+        print(f"iters:\n{iters}")
+        print(f"flags:\n{flags}")
+    # test for convergence. must always work with near the money options
+    assert sum(successes) == n_pts
+
+
+@pytest.mark.parametrize("method", ["halley", "newton"])
+@pytest.mark.parametrize("guess", [0.5, 0.7, 1]) # default guess is 0.5
+@pytest.mark.parametrize("max_pts", [np.inf])
+@pytest.mark.parametrize("py_debug,c_debug", [(False, False)])
+def test_c_ntm_bachelier_vol(options_ntm_data, rf_stop_defaults, method, guess,
+                             max_pts, py_debug, c_debug):
+    """Test solving for Bachelier vols using :func:`options_ntm_data` data.
+    
+    Directly calls the ``_bachelier_vol`` function from ``_ivlib.so``.
+    
+    :param options_ntm_data: :func:`options_ntm_data` ``pytest`` fixture.
+    :type options_ntm_data: :class:`numpy.ndarray`
+    :param rf_stop_defaults: :func:``rf_stop_defaults` ``pytest`` fixture.
+    :type rf_stop_defaults: tuple
+    :param method: Method to use to solve. Only supports ``"halley"`` and
+        ``"newton"`` currently. These will be converted to :class:`bytes`
+        objects in the function to be suitable for passing to a C function.
+    :type method: str
+    :param guess: Starting guess for Bachelier implied volatility.
+    :type guess: float
+    :param max_pts: Maximum number of data points (rows) from
+        :func:`options_ntm_data` to run tests with. Must be positive, and if
+        larger than ``options_ntm_data.shape[0]``, will be set to
+        ``options_ntm_data.shape[0]`` automatically.
+    :type max_pts: int
+    :param py_debug: ``True`` for debugging output (print statements) from
+        Python interpreter, ``False`` for silence. Useful for debugging large
+        chunks of data points at once.
+    :type py_debug: bool
+    :param c_debug: ``True`` for debugging output (print to ``stdout``) from C
+        function, ``False`` for silence except upon error. Useful for debugging
+        single data points.
+    :type c_debug: bool
+    """
+    # convert ndarray options_ntm_data to tuple of vol_obj_args
+    voas = ndarray2vol_obj_args_tuple(options_ntm_data)
+    # convert method to int
+    method = 0 if "halley" else (1 if "newton" else -1)
+    assert method in (0, 1), "method flag must be 0 (halley) or 1 (newton)"
+    # change method to int since method is determined with flags
+    # get min of max_pts and length of voas
+    n_pts = min(max_pts, len(voas))
+    # implied volatility results (struct), values (vols), convergences, flags
+    volrs = [None for _ in range(n_pts)]
+    vols = np.zeros(n_pts)
+    successes = np.zeros(n_pts).astype(bool)
+    # can't use numpy.zeros(n_pts).astype(str) since the string gets clipped.
+    # maybe raise an issue with the numpy maintainers?
+    flags = [None for _ in range(n_pts)]
+    # iterations per point solved
+    iters = np.zeros(n_pts)
+    # for the first n_pts, find the black vol
+    for i in range(n_pts):
+        vol_args = voas[i]
+        # get result struct
+        volrs[i] = _ivlib._bachelier_vol(ctypes.byref(vol_args), method,
+                                         guess, *rf_stop_defaults, c_debug)
+        # get actual vol value
+        vols[i] = volrs[i].res
+        # get convergence information (True if converged)
+        successes[i] = volrs[i].converged
+        # reason for failure/success (decode bytes object)
+        flags[i] = volrs[i].flag.decode()
+        # number of iterations required
+        iters[i] = volrs[i].iters
+    # if py_debug, print vols, convergences, number of iterations, and flags
+    if py_debug == True:
+        print(f"vols:\n{vols}")
+        print(f"successes:\n{successes}")
+        print(f"iters:\n{iters}")
+        print(f"flags:\n{flags}")
+    # test for convergence. must always work on near the money options
     assert sum(successes) == n_pts
