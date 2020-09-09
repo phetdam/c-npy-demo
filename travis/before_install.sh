e787e1209aa308fad7167138d689883c0c514d2d
Derek Huang <djh458@stern.nyu.edu>
2020-09-09 04:16:59 -0400

    rename build_ext.sh to build_all.sh, rewrite all bash script so source/no source is irrelevant, add run_tests.sh to run pytest and setuptools-generated scripts (upon install), update .travis.yml to use new run_tests.sh script

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/bash
 # upgrade required packages. use in before_install in .travis.yml
-# note: DO NOT source this script.
+
+# before install function
+before_install() {
+    # install newest gcc
+    sudo apt-get update
+    sudo apt-get install gcc
+    # check python version and upgrade required tools
+    python3 --version
+    pip3 install --upgrade pip
+    pip3 install --upgrade pytest
+    pip3 install --upgrade setuptools
+    pip3 install --upgrade wheel
+}
 
 # if not on travis ci, exit
 if ! [ $TRAVIS ]
 then
     echo "WARNING: intended for execution on Travis CI only. exiting"
-    exit
+else
+    # run pre-install commands
+    before_install
 fi
-
-# install newest gcc
-sudo apt-get update
-sudo apt-get install gcc
-# check python version and upgrade required tools
-python3 --version
-pip3 install --upgrade pip
-pip3 install --upgrade pytest
-pip3 install --upgrade setuptools
-pip3 install --upgrade wheel
---
+++ travis/run_tests.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+# run tests on built package using pytest, install into root with pip3, and then
+# run the setuptools-generated scripts verbosely as test.
+
+# test built package with pytest and then test setuptools-generated benchmarks
+run_tests() {
+    # run test suite; tests for setuptools-generated scripts are skipped
+    pytest
+    # install
+    make install
+    # run extension and vol benchmarks, verbosely (with defaults)
+    python3 c_npy_demo.bench.ext -v
+    python3 c_npy_demo.bench.vol -v
+}
+
+# check if on travis or not
+if ! [ $TRAVIS ]
+then
+    echo "WARNING: script intended to be run on Travis CI"
+else
+    # check that pkg_test directory exists; if not print error and exit
+    if ! [ -d pkg_test ]
+    then
+        echo "error: cannot find directory pkg_test. exiting"
+    else
+        # run all tests
+        run_tests
+    fi
+fi
