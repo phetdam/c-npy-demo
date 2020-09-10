6b69792831b83eb4080705a3898948dba20b6df2
Derek Huang <djh458@stern.nyu.edu>
2020-09-10 05:31:06 -0400

    correct typo in build_dist.sh in docker install/test

--- travis/build_dist.sh
+++ travis/build_dist.sh
@@ -16,9 +16,9 @@ build_cp3_wheels() {
         # only accept python versions 3.6-3.8
         if $PY_BIN/python3 --version | grep "3\.[6-8]\.[0-9]"
         then
-            # build wheel for this python version. first install dependencies from
-            # travis/requirements.txt, then run sdist bdist_wheel. this is because
-            # we mounted repository home to DOCKER_MNT. use only binary dist.
+            # build wheel for this python version. first install dependencies 
+            # from travis/requirements.txt, then run sdist bdist_wheel. this is
+            # because we mounted repo home to DOCKER_MNT. use only binary dist.
             $PY_BIN/pip3 install -r $DOCKER_MNT/travis/requirements.txt \
                 --only-binary :all:
             # if can't find python3 use absolute path
@@ -70,13 +70,19 @@ else
     # repair wheels using auditwheel in manylinux1 image
     repair_cp3_wheels $DOCKER_MNT/dist
     # for each python version, install from wheel and run benchmarks
-    for $PY_BIN in /opt/python/cp3*/bin
+    for PY_BIN in /opt/python/cp3*/bin
     do
-        $PY_BIN/pip install c_npy_demo --no-index --only-binary :all: \
-            -f $DOCKER_MNT/dist
-        cd $HOME
-        # run extension and vol benchmarks, verbosely (with defaults)
-        c_npy_demo.bench.ext -v
-        c_npy_demo.bench.vol -v
+        # only accept python versions 3.6-3.8
+        if $PY_BIN/python3 --version | grep "3\.[6-8]\.[0-9]"
+        then
+            $PY_BIN/pip install c_npy_demo --no-index --only-binary :all: \
+                -f $DOCKER_MNT/dist
+            cd $HOME
+            # run extension and vol benchmarks, verbosely (with defaults)
+            c_npy_demo.bench.ext -v
+            c_npy_demo.bench.vol -v
+        else
+            echo "no wheel will be built for `$PY_BIN/python3 --version`"
+        fi
     done
 fi
