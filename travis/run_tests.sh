d9e97785cf58b2dccf5fbbae7b3f73634f17bc61
Derek Huang <djh458@stern.nyu.edu>
2020-09-10 23:04:05 -0400

    fix incorrect assignment to PY_VER in run_tests.sh

--- travis/run_tests.sh
+++ travis/run_tests.sh
@@ -17,8 +17,8 @@ run_venv_tests() {
     # install requirements from requirements.txt
     $PY_BIN/pip3 install -r $DOCKER_MNT/travis/requirements.txt
     # get last major + minor python version number
-    PY_VER=$PY_BIN/python3 --version | sed -e s/"Python "// -e s/"\.[0-9]$"// \
-        -e s/"\."//
+    PY_VER=`$PY_BIN/python3 --version | sed -e s/"Python "// -e s/"\.[0-9]$"//`
+    PY_VER=`echo $PY_BIN | sed -e s/"\."//`
     # try to find the wheel; if it can't be found, skip
     if find $WHEEL_DIR/c_npy_demo-*$PY_VER-$PY_VER*-manylinux*.whl
     then
