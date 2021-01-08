0bce15e464e2515f1fd2e6702658330023c3d074
Derek Huang <djh458@stern.nyu.edu>
2021-01-07 20:46:23 -0500

    change CK_NORMAL to CK_ENV; maybe Makefile can set CK_VERBOSITY externally for flexibility in showing output

--- check/runner.c
+++ check/runner.c
@@ -43,9 +43,10 @@ int main(int argc, char **argv) {
   }
   // instantiate our test suite. note this does not have to be freed!
   Suite *suite = pytest_suite();
-  // create our suite runner and run all tests (CK_NORMAL -> only show failed)
+  // create our suite runner and run all tests (CK_ENV -> set CK_VERBOSITY and
+  // if not set, default to CK_NORMAL, i.e. only show failed)
   SRunner *runner = srunner_create(suite);
-  srunner_run_all(runner, CK_NORMAL);
+  srunner_run_all(runner, CK_ENV);
   // get number of failed tests and free runner
   int n_failed = srunner_ntests_failed(runner);
   srunner_free(runner);
