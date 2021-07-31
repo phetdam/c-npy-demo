c34a3f86562c15dff096981488ce269153587d14
Derek Huang <djh458@stern.nyu.edu>
2021-07-30 22:44:20 -0400

    functimer/__init__.py: remove quotes from final part of docstring description

--- npapibench/functimer/__init__.py
+++ npapibench/functimer/__init__.py
@@ -4,7 +4,7 @@ Inspired by ``timeit`` but times callables with arguments without using
 executable string statements. This way, timings of several callables that share
 the same args avoids multiple setup statement calls.
 
-"Times obtained by default using time.perf_counter. Functional API only."
+Times obtained by default using time.perf_counter. Functional API only.
 
 .. codeauthor:: Derek Huang <djh458@stern.nyu.edu>
 """
